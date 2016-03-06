12d480c1b11144938f47e9cf6c84ed14b3530431
Anthony Grimes <i@raynes.me>
2016-03-06 14:56:15 -0800

    Pinned version of alexandra

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     include_package_data=True,
     url="https://github.com/Raynes/yams",
     install_requires=[
-        'rxv==0.1.9'
+        'rxv==0.1.9',
+        'alexandra==0.3.0'
     ],
     packages=find_packages()
 )
